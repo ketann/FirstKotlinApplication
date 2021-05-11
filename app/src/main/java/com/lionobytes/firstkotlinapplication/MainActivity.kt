764f8328ca9d61f98b3a5e89e4fb90e3b557275d
Ketan Nakum <dhaval@lionobytes.com>
2021-05-11 19:39:51 +0530

    add birthday cake in println message.

--- app/src/main/java/com/lionobytes/firstkotlinapplication/MainActivity.kt
+++ app/src/main/java/com/lionobytes/firstkotlinapplication/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("Happy Birthday, Jone")
+        println("you are alredy 5")
+        println("   ,,,,,   ")
+        println("   |||||   ")
+        println(" =========")
+        println("@@@@@@@@@@@")
+        println("{~@~@~@~@~}")
+        println("@@@@@@@@@@@")
+        println("")
+        println("5 is the very best age to celebrate! ")
     }
 }
